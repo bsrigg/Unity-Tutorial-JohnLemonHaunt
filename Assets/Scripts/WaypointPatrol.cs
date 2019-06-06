fa83923436e1d1959daab866e975abf4474fb24f
Barry S Rigg <barrysrigg@gmail.com>
2019-06-06 10:03:13 -0400

    Uploading the things

---
+++ Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointPatrol : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    public Transform[] waypoints;
+    int m_CurrentWaypointIndex;
+    int m_Reverse;
+
+    void Start()
+    {
+        navMeshAgent.SetDestination(waypoints[0].position);
+        navMeshAgent.stoppingDistance = 0.25f;
+    }
+
+    void Update()
+    {
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
+            navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
+        }
+
+    }
+}
